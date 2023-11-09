7f8c16e1a9414eb8d7919e32f3bdadc72ff8a844
Emmakjeld <126453240+Emmakjeld@users.noreply.github.com>
2023-11-09 13:18:20 +0100

    Update på kode

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ window.setTimeout(fjernSplash,3000);
 function fjernSplash (){
     splash.style.animationName= "fadeOut";
 }
+
+const btn_snak = document.getElementById("billedeafann");
+
+btn_snak.onmouseover = function(){
+    audio_ann.play();
+}
+
+btn_snak.onmouseout = function(){
+    audio_ann.pause();
+}
