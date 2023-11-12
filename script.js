53e553124cd95aa4c2669783e582c0c0c09da73a
Emmakjeld <126453240+Emmakjeld@users.noreply.github.com>
2023-11-12 22:03:03 +0100

    Update på kode

--- script.js
+++ script.js
@@ -14,3 +14,10 @@ btn_snak.onmouseover = function(){
 btn_snak.onmouseout = function(){
     audio_ann.pause();
 }
+
+let billedekarussel = document.getElementById("billedekarussel");
+let billeder = ['karussel1.jpg','karussel2.jpg','karussel3.jpg']
+setInterval(function(){
+    let random = Math.floor(Math.random() * 3);
+    billedekarussel.src = billeder[random];
+}, 800);
