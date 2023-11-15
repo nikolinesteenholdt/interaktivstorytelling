31d69bd7c529b055a8b73b30cea63bfc187b021d
Emmakjeld <126453240+Emmakjeld@users.noreply.github.com>
2023-11-15 13:15:59 +0100

    Kode færdig

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const splash = document.getElementById("splash");
+/*const splash = document.getElementById("splash");
 
 window.setTimeout(fjernSplash,3000);
 function fjernSplash (){
     splash.style.animationName= "fadeOut";
-}
+}*/
 
 const btn_snak = document.getElementById("billedeafann");
 
@@ -15,9 +15,30 @@ btn_snak.onmouseout = function(){
     audio_ann.pause();
 }
 
+let nuBillede = 0;
 let billedekarussel = document.getElementById("billedekarussel");
-let billeder = ['karussel1.jpg','karussel2.jpg','karussel3.jpg']
+const galleri1 = document.getElementById("galleri1");
+const galleri2 = document.getElementById("galleri2");
+const galleri3 = document.getElementById("galleri3");
+
 setInterval(function(){
-    let random = Math.floor(Math.random() * 3);
-    billedekarussel.src = billeder[random];
-}, 800);
+    if(nuBillede==0){
+        galleri1.style.display = "inline";
+        galleri2.style.display = "none";
+        galleri3.style.display = "none";
+        nuBillede+=1;
+    }
+    else if(nuBillede==1){
+        galleri1.style.display = "none";
+        galleri2.style.display = "inline";
+        galleri3.style.display = "none";
+        nuBillede+=1;
+    }
+    else if(nuBillede==2){
+        galleri1.style.display = "none";
+        galleri2.style.display = "none";
+        galleri3.style.display = "inline";
+        nuBillede=0;
+    }    
+    }, 3000);
+
